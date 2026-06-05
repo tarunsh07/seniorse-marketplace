159509fea7febc90c3870d235f3c1d494c3d3f43
Tarun Sharma <tarun.sh.work@gmail.com>
2026-06-06 02:00:28 +0530

    fixed e-mail content

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,7 +2,9 @@ const nodemailer = require("nodemailer");
 
 const sendOtpEmail = async (email, otp) => {
     const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
